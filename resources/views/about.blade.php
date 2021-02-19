c3d1ddc49e84a70cd77045b00f86d9f0028387a0
fachrizalafandi <fachrizalafandi13@gmail.com>
2021-02-19 11:38:08 +0700

    menambah route baru & menambah view about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,7 +9,7 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-  <title>Belajar Laravel</title>
+  <title>Halaman About</title>
 </head>
 
 <body>
@@ -38,7 +38,7 @@
   <div class="container">
     <div class="row">
       <div class="col-10">
-        <h1 class="mt-3">Hello, world!</h1>
+        <h1 class="mt-3">Hello, Rizal!</h1>
       </div>
     </div>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/mahasiswa', function () {
+    return view('mahasiswa');
+});
