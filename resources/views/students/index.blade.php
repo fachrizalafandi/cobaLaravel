b583831e16daf3362004cd34c0ff308e7de347ca
fachrizalafandi <fachrizalafandi13@gmail.com>
2021-02-23 22:45:47 +0700

    edit width kolom jurusan di datatable

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -3,19 +3,19 @@
 @section('title','Daftar Mahasiswa')
 @section('container')
 <div class="container">
-    <div class="row">
+  <div class="row">
         <div class="col-12">
             <h1 class="mt-3">Daftar Mahasiswa</h1>
             <div class="card">
                 <div class="card-body">
                     <table id="example" class="table table-striped table-bordered" style="width:100%">
-                        <thead>
+                        <thead class="thead-dark">
                             <tr>
                                 <th>#</th>
                                 <th>Nama</th>
                                 <th>NIM</th>
                                 <th>Email</th>
-                                <th>Jurusan</th>
+                                <th style="width: 120px">Jurusan</th>
                                 <th></th>
                             </tr>
                         </thead>
@@ -28,9 +28,9 @@
                                 <td>{{ $student->email }}</td>
                                 <td>{{ $student->jurusan }}</td>
                                 <td>
-                                    <a href="#" class="btn btn-outline-dark btn-sm"><i class="fas fa-info-circle"></i></a>
-                                    <a href="#" class="btn btn-outline-success btn-sm"><i class="fas fa-edit"></i></a>
-                                    <a href="#" class="btn btn-outline-danger btn-sm"><i class="fas fa-trash-alt"></i></a>
+                                    <a href="#" class="btn btn-outline-dark btn-sm mt-1"><i class="fas fa-info-circle"></i></i></a>
+                                    <a href="#" class="btn btn-outline-success btn-sm mt-1"><i class="far fa-edit"></i></a>
+                                    <a href="#" class="btn btn-outline-danger btn-sm mt-1"><i class="far fa-trash-alt"></i></a>
                                 </td>
                             </tr>
                         @endforeach
